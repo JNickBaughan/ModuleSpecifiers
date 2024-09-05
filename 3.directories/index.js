f4a752630ad5df5ec3f9a4354f64a38296ff3ef8
J Nick Baughan <JNickBaughan@gmail.com>
2024-09-04 20:27:21 -0400

    add new example

---
+++ 3.directories/index.js
@@ -0,0 +1,6 @@
+import getAreaCircle from "./circle-calculator";
+
+const radius = 3;
+const area = getAreaCircle(radius)
+console.log(`cirlce area = ${area}`);
+
--- 3.nested-directories/index.js
+++
@@ -1,5 +0,0 @@
-import { add } from "./calculator/operations/add";
-import { subtract } from "./calculator/operations/subtract";
-import { multiply } from "./calculator/operations/multiply";
-import { divide } from "./calculator/operations/divide";
-import { pi } from "./calculator/constants/pi";
